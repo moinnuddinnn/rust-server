61ff3fdf75b4520886a75575f0ffbf401f1030f3
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-22 14:17:05 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ mod routes;
 mod models;
 
 use actix_files as fs;
-use actix_web::{App, HttpServer, web};
-use tera::Tera;
+use actix_web::{App, HttpServer, web}; //main actix imports
+use tera::Tera; //tera imports used for templating html files
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // initialize tera (template engine)
+    // initializing tera
     let tera = Tera::new("templates/**/*").expect("Error loading templates");
 
-    println!("🚀 Server running at http://127.0.0.1:8080");
+    println!("Server running at http://127.0.0.1:8080");
 
     HttpServer::new(move || {
         App::new()
