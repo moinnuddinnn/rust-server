43866afb53f030c872520214201f5735fe01e8d9
iftekhar moinuddin <134924046+moinnuddinnn@users.noreply.github.com>
2025-10-22 14:18:23 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,15 @@ use tera::Tera; //tera imports used for templating html files
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // initializing tera
+    //initializing tera
     let tera = Tera::new("templates/**/*").expect("Error loading templates");
     println!("Server running at http://127.0.0.1:8080");
 
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(tera.clone())) // share tera with handlers
-            .service(fs::Files::new("/static", "static").show_files_listing()) // serve static files
-            .configure(routes::init) // load all routes
+            .app_data(web::Data::new(tera.clone())) //share tera with handlers
+            .service(fs::Files::new("/static", "static").show_files_listing()) //serve static files
+            .configure(routes::init)// load all routes
     })
     .bind(("127.0.0.1", 8080))?
     .run()
